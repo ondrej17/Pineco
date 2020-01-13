b4c776cc088a2a140cb09c4389d2394bf760e227
ondrej17 <bily.ondrej1701@gmail.com>
2020-01-13 18:37:00 +0100

    Problem with java version solved

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 ## Pineco
 
 An app that takes notes, organizes your life and so on.
-
-It is not working yet. In near future, it shall be remade using javafx library.
-
-Tutorial for javafx is here:
-https://docs.gluonhq.com/scenebuilder/
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
+        primaryStage.setTitle("Pineco");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
